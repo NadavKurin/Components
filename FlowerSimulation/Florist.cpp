57db66ef162837f366670bb02b8dab3071fd805d
oranbn <oranbenn@post.bgu.ac.il>
2021-05-13 00:31:08 +0300

    update v3.01

--- FlowerSimulation/Florist.cpp
+++ FlowerSimulation/Florist.cpp
@@ -12,8 +12,11 @@ Florist::Florist(std::string name, Wholesaler* w, FlowerArranger* f, DeliveryPer
 void Florist::acceptOrder(Person* person, std::vector<std::string> order)
 {
 	// TODO
-	std::cout << getName() << " forwards request to Wholesaler " << wholesaler->getName() << std::endl;
+	std::cout << getName() << " forwards request to " << wholesaler->getName() << std::endl;
 	FlowersBouquet* flowersBouquet = wholesaler->acceptOrder(order);
+	std::cout << wholesaler->getName() << " returns flowers to " << getName() << std::endl;
+	std::cout << getName() << " request flowers arragement from " << flowerArranger->getName() << std::endl;
+	
 	flowerArranger->arrangeFlowers(flowersBouquet);
 	std::cout << flowerArranger->getName() << " returns arranged flowers to " << getName() << std::endl;
 	std::cout << getName() << " forwards flowers to " << deliveryPerson->getName() << std::endl;
--- FlowerSimulation/Person.cpp
+++ FlowerSimulation/Person.cpp
@@ -12,16 +12,21 @@ std::string Person::getName()
 
 void Person::orderFlowers(Florist* florist, Person* person, std::vector<std::string> order)
 {
-	std::cout << getName() << " orders flowers to " << person->getName() << " from " << florist->getName() << std::endl;
+	std::string flowers = " ";
+	for(auto& elem : order)
+	{
+		flowers = flowers + elem + ", ";
+	}
+	std::cout << getName() << " orders flowers to " << person->getName() << " from " << florist->getName() <<":" << flowers << std::endl;
 	florist->acceptOrder(person, order);
 }
 
 void Person::acceptFlower(FlowersBouquet* flowersBouquet)
-{
+{ 
 	std::string output ="";
 	for (auto& elem : flowersBouquet->getBouquet())
 	{
 		output = output + elem + ", ";
 	}
-	std::cout << getName() <<"accepts the flowers: " << output << std::endl;
+	std::cout << getName() <<" accepts the flowers: " << output << std::endl;
 }
