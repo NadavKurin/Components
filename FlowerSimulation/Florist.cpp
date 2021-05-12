2cd3e08685791877aa50bcb147ca0864f3cd39bf
oranbn <oranbenn@post.bgu.ac.il>
2021-05-12 21:51:29 +0300

    update

--- FlowerSimulation/Florist.cpp
+++ FlowerSimulation/Florist.cpp
@@ -2,13 +2,17 @@
 
 #include "Person.h"
 
-Florist::Florist(std::string name) : Person(name)
+Florist::Florist(std::string name, Wholesaler* w, FlowerArranger* f, DeliveryPerson* d) : Person(name)
 {
+	wholesaler = w;
+	flowerArranger = f;
+	deliveryPerson = d;
 }
 
 void Florist::acceptOrder(Person* person, std::vector<std::string> order)
 {
 	// TODO
+	std::cout << getName() << " forwards request to Wholesaler " << wholesaler->getName() << std::endl;
 }
 
 std::string Florist::getName() {
--- FlowerSimulation/Florist.h
+++ FlowerSimulation/Florist.h
@@ -3,15 +3,21 @@
 #include <vector>
 
 #include "Person.h"
+#include "Wholesaler.h"
+#include "FlowerArranger.h"
+#include "DeliveryPerson.h"
 
 class Person;
 
 class Florist : public Person
 {
 private:
+    Wholesaler* wholesaler;
+    FlowerArranger* flowerArranger;
+    DeliveryPerson* deliveryPerson;
 
 public:
-    Florist(std::string);
+    Florist(std::string, Wholesaler* w, FlowerArranger* f, DeliveryPerson* d);
     void acceptOrder(Person*, std::vector<std::string>);
     std::string getName();
 };
