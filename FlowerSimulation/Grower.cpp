ee41774c72111b4702c613aa07da72b43d4fa8a8
oranbn <oranbenn@post.bgu.ac.il>
2021-05-12 23:01:19 +0300

    update

--- FlowerSimulation/Grower.cpp
+++ FlowerSimulation/Grower.cpp
@@ -1,1 +1,19 @@
 #include "Grower.h"
+
+#include "Gardener.h"
+#include "Person.h"
+
+Grower::Grower(Gardener* g): Person(name)
+{
+	gardener = g;
+}
+
+FlowersBouquet Grower::prepareOrder(std::vector<std::string> order)
+{
+	std::cout << getName() << " forwards the request to Gardener " << gardener->getName() << std::endl;
+	return gardener->prepareBouquet(order);
+}
+
+std::string Grower::getName() {
+	return "Grower " + Person::getName();
+}
