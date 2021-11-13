073eea683e24ca91a8bee4d8d70bc816401bec90
Nadav <nadavku@post.bgu.ac.il>
2021-11-13 12:01:06 +0200

    ש

--- BitwiseMultiwayDemux.cs
+++ BitwiseMultiwayDemux.cs
@@ -5,67 +5,29 @@ using System.Text;
 
 namespace Components
 {
-    //This class implements a demux with k outputs, each output with n wires. The input also has n wires.
-
-    class BitwiseMultiwayDemux : Gate
+    class BitwiseMultiwayDemux : Gate,Component
     {
-        //Word size - number of bits in each output
         public int Size { get; private set; }
-
-        //The number of control bits needed for k outputs
         public int ControlBits { get; private set; }
-
         public WireSet Input { get; private set; }
         public WireSet Control { get; private set; }
         public WireSet[] Outputs { get; private set; }
 
-        //your code here
-        private BitwiseDemux[] bitwiseDemuxes;
         public BitwiseMultiwayDemux(int iSize, int cControlBits)
         {
             Size = iSize;
             Input = new WireSet(Size);
             Control = new WireSet(cControlBits);
             Outputs = new WireSet[(int)Math.Pow(2, cControlBits)];
+
             for (int i = 0; i < Outputs.Length; i++)
             {
                 Outputs[i] = new WireSet(Size);
             }
-            //your code here
-            bitwiseDemuxes = new BitwiseDemux[(int)Math.Pow(2, cControlBits) - 1];
-            for (int i = 0; i < bitwiseDemuxes.Length; i++)
-            {
-
-                bitwiseDemuxes[i] = new BitwiseDemux(Size);
-            }
-
-
-            for (int j = 0; j < Outputs.Length; j += 2)
-            {
-                Outputs[j].ConnectInput(bitwiseDemuxes[j / 2].Output1);
-                Outputs[j+1].ConnectInput(bitwiseDemuxes[j / 2].Output2);
-            }
-
-            for (int i = Outputs.Length/2, j = 0; i < Outputs.Length-1; i = i + 1, j = j + 2)
-            {
-                bitwiseDemuxes[j].ConnectInput(bitwiseDemuxes[i].Output1);
-                bitwiseDemuxes[j+1].ConnectInput(bitwiseDemuxes[i].Output2);
-            }
-            bitwiseDemuxes[bitwiseDemuxes.Length - 1].ConnectInput(Input);
-
-            //now we'll connect the controls to the demux gate
-            int controlNumber = Control.Size-1; 
-            int demuxNumber = bitwiseDemuxes.Length - 1; 
-            for (int i = 0, j = 1; i < Control.Size; i++, j = j * 2) 
-            {
-                for(int k = 0; k < j; k++)
-                {
-                    bitwiseDemuxes[demuxNumber].Control.ConnectInput(Control[controlNumber]);
-                    demuxNumber--;
-                }
-                controlNumber--; 
-            }
-
+            for (int i = 0; i < Size; i++)
+                Input[i].ConnectOutput(this);
+            for(int i = 0; i < cControlBits; i++)
+                Control[i].ConnectOutput(this);
         }
 
 
@@ -78,47 +40,45 @@ namespace Components
             Control.ConnectInput(wsControl);
         }
 
-
         public override bool TestGate()
         {
-            //setting the Outputs
-            for (int i = 0; i < Math.Pow(2, ControlBits); i++)
-            {
-                int[] binaryI = makeBinary(i, Size);
-                for (int j = 0; j < binaryI.Length; j++)
-                    Outputs[i][j].Value = binaryI[j];
-            }
+            Random rnd = new Random();
+            int iValue = rnd.Next((int)Math.Pow(2,Size));
+            Input.SetValue(iValue);
 
-            for (int i = 0; i < Math.Pow(2, ControlBits); i++)
+            for (int i = 0; i < Outputs.Length; i++)
             {
-                int[] binaryI = makeBinary(i, ControlBits);
-                for (int j = 0; j < binaryI.Length; j++)
+                Control.SetValue(i);
+                
+                for (int j = 0; j < Outputs.Length; j++)
                 {
-                    Control[i].Value = binaryI[j];
-                }
-
-                for (int j = 0; j < Size; j++)
-                {
-                    if (Input[j].Value != Outputs[i][j].Value)
+                    if (j == i)
+                    {
+                        if (Outputs[j].GetValue() != iValue)
+                            return false;
+                    }
+                    else if (Outputs[j].GetValue() != 0)
+                    {
                         return false;
+                    }
                 }
             }
-
             return true;
         }
 
+        #region Component Members
 
-        public static int[] makeBinary(int num, int length)
+        public void Compute()
         {
-            string b = Convert.ToString(num, 2);
-            while (b.Length < length)
-                b = "0" + b;
-
-            int[] binaryNumInArray = new int[length];
-            for (int i = 0; i < length; i++)
-                binaryNumInArray[i] = int.Parse(b[i].ToString());
-
-            return binaryNumInArray;
+            for (int i = 0; i < Outputs.Length; i++)
+            {
+                if (i == Control.GetValue())
+                    Outputs[i].SetValue(Input.GetValue());
+                else
+                    Outputs[i].SetValue(0);
+            }
         }
+
+        #endregion
     }
 }
--- BitwiseMultiwayMux.cs
+++ BitwiseMultiwayMux.cs
@@ -5,71 +5,33 @@ using System.Text;
 
 namespace Components
 {
-    //This class implements a mux with k input, each input with n wires. The output also has n wires.
-
-    class BitwiseMultiwayMux : Gate
+    class BitwiseMultiwayMux : Gate, Component
     {
-        //Word size - number of bits in each output
         public int Size { get; private set; }
-
-        //The number of control bits needed for k outputs
         public int ControlBits { get; private set; }
-
         public WireSet Output { get; private set; }
         public WireSet Control { get; private set; }
         public WireSet[] Inputs { get; private set; }
 
-        //your code here
 
-        private BitwiseMux[] bitwiseMuxes;
         public BitwiseMultiwayMux(int iSize, int cControlBits)
         {
+            ControlBits = cControlBits;//bug fix
+
             Size = iSize;
             Output = new WireSet(Size);
             Control = new WireSet(cControlBits);
             Inputs = new WireSet[(int)Math.Pow(2, cControlBits)];
-            
+
             for (int i = 0; i < Inputs.Length; i++)
             {
                 Inputs[i] = new WireSet(Size);
+                for(int j = 0; j < Inputs[i].Size; j++)
+                    Inputs[i][j].ConnectOutput(this);
             }
+            for(int i = 0; i < cControlBits; i++)
+                Control[i].ConnectOutput(this);
 
-            //your code here
-            bitwiseMuxes = new BitwiseMux[(int)Math.Pow(2, cControlBits) - 1];
-            for(int i = 0; i < bitwiseMuxes.Length; i++)
-            {
-                bitwiseMuxes[i] = new BitwiseMux(Size);
-            }
-
-            /*The idea is that We'll connect every two inputs to mux and every mux will decide which one "wins"
-             * the next stage we'll remain with Size/2 inputs and we'll do the same on them, connect every two inputs to mux
-             * and so on, at the end we will have only two outputs "at the final" and we will connect them to mux that will decide who will be the winner input and he will be the output
-             * We'll connect the Inputs to the first bitwiseMuxes.length/2 bitwise muxeses.*/
-
-            for(int i = 0, j = 0; i < bitwiseMuxes.Length/2 + 1; i++, j = j + 2)
-            {
-                bitwiseMuxes[i].ConnectInput1(Inputs[j]);
-                bitwiseMuxes[i].ConnectInput2(Inputs[j+1]);  
-            }
-
-
-            for(int i = bitwiseMuxes.Length / 2 + 1, j = 0; i < bitwiseMuxes.Length ; i = i + 1, j = j + 2)
-            {
-                bitwiseMuxes[i].ConnectInput1(bitwiseMuxes[j].Output);
-                bitwiseMuxes[i].ConnectInput2(bitwiseMuxes[j + 1].Output);
-            }
-            Output.ConnectInput(bitwiseMuxes[bitwiseMuxes.Length - 1].Output);
-
-            //now we'll connect the controls to the mux gate
-            int muxCounter = 0;
-            for(int i = 0, j = (int)Math.Pow(2, Control.Size -1); i < Control.Size; i++, j = j / 2) 
-            {
-                for(int k = 0; k < j; k++)
-                {
-                    bitwiseMuxes[muxCounter].ConnectControl(Control[i]); 
-                    muxCounter++; 
-                }
-            }
         }
 
 
@@ -82,49 +44,38 @@ namespace Components
             Control.ConnectInput(wsControl);
         }
 
-
-
-
         public override bool TestGate()
         {
-
-            //setting the inputs
-            for(int i = 0; i < Math.Pow(2,ControlBits); i++)
+            Random rnd = new Random();
+            WireSet[] aInputs = new WireSet[Inputs.Length];
+            for (int i = 0; i < Inputs.Length; i++)
             {
-                int[] binaryI = makeBinary(i, Size);
-                for (int j = 0; j < binaryI.Length; j++)
-                    Inputs[i][j].Value = binaryI[j];
+                aInputs[i] = new WireSet(Size);
+                ConnectInput(i, aInputs[i]);             
+                aInputs[i].SetValue(i + 1);
             }
-
-            for(int i = 0; i < Math.Pow(2, ControlBits); i++)
+            WireSet wsControl = new WireSet(ControlBits);
+            ConnectControl(wsControl);
+            for (int i = 0; i < Inputs.Length; i++)
             {
-                int[] binaryI = makeBinary(i, ControlBits);
-                for (int j = 0; j < binaryI.Length; j++) {
-                    Control[i].Value = binaryI[j];
-                }
 
-                for (int j = 0; j < Size; j++)
-                {
-                    if (Output[j].Value != Inputs[i][j].Value)
-                        return false;
-                }
+                wsControl.SetValue(i);
+                
+                if (Output.GetValue() != Inputs[i].GetValue())
+                    return false;
             }
-
             return true;
         }
 
+        #region Component Members
 
-        public static int[] makeBinary(int num, int length)
+        public void Compute()
         {
-            string b = Convert.ToString(num, 2);
-            while (b.Length < length)
-                b = "0" + b;
-
-            int[] binaryNumInArray = new int[length];
-            for (int i = 0; i < length; i++)
-                binaryNumInArray[i] = int.Parse(b[i].ToString());
-
-            return binaryNumInArray;
+            Output.SetValue(Inputs[Control.GetValue()].GetValue());
         }
+
+        #endregion
+
+
     }
 }
